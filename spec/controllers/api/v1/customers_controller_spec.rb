5669cd00c5d4061b54076aa1bf9bd591cc890730
Pat Wey <patrickwey@gmail.com>
2016-01-04 17:32:39 -0700

    Implement #index and #show for customers_controller

---
+++ spec/controllers/api/v1/customers_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::CustomersController, type: :controller do
+  def create_customers(n)
+    n.times do |n|
+      Customer.create!(first_name: "First #{n}",
+                       last_name:  "Last #{n}")
+    end
+  end
+
+  describe "get #index" do
+    it "returns all of the customers" do
+      create_customers(3)
+
+      get :index, format: :json
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(Customer.all.to_json)
+    end
+  end
+
+  describe "get #show" do
+    it "returns the customer with given id" do
+      m = Customer.create!(first_name: "First",
+                           last_name:  "Last")
+
+      get :show, id: m.id, format: :json
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(Customer.find(m.id).to_json)
+    end
+  end
+end
