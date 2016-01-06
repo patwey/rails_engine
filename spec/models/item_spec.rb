8edb14aa8bf9c3b1a14819daface06a1fcf818e4
Pat Wey <patrickwey@gmail.com>
2016-01-06 16:03:57 -0700

    Remove unused tests

--- spec/models/item_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Item, type: :model do
-  describe "#most_items" do
-    it "returns the top x item instances ranked by total number sold" do
-      skip
-    end
-  end
-end
