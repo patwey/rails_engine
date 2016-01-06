827b557e4a3f62b5422adc6d7b5bd1947f9f4b31
Pat Wey <patrickwey@gmail.com>
2016-01-05 19:16:08 -0700

    finish testing relationship-endpoints

--- spec/controllers/api/v1/customers/transactions_controller_spec.rb
+++ spec/controllers/api/v1/customers/transactions_controller_spec.rb
@@ -2,20 +2,26 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::Customers::TransactionsController, type: :controller do
   describe "get #index" do
-    xit "responds to json" do
-      # create stuff
+    it "responds to json" do
+      c = Customer.create!(first_name: "Customer")
+      i = Invoice.create!(customer_id: c.id)
+      Transaction.create!(invoice_id: i.id)
 
       get :index, id: c.id, format: :json
 
       expect(response.status).to eq(200)
     end
 
-    xit "returns transactions for the customer with the given customer id" do
-      # create stuff
+    it "returns transactions for the customer with the given customer id" do
+      c = Customer.create!(first_name: "Customer")
+      i = Invoice.create!(customer_id: c.id)
+      i2 = Invoice.create!(customer_id: c.id)
+      Transaction.create!(invoice_id: i.id)
+      Transaction.create!(invoice_id: i2.id)
 
       get :index, id: c.id, format: :json
 
-      expect(response.body).to eq(Customer.find(c.id).invoices.to_json)
+      expect(response.body).to eq(Customer.find(c.id).transactions.to_json)
     end
   end
 end
